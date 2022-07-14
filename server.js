30e9d4359844596f95ff9cc841445811a44d7a75
masa-1234tf <deathjuly88@gmail.com>
2022-07-15 00:15:42 +0900

    add

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var port = process.env.PORT
 
 var express = require("express")
 var app = express()
-
+app.use("/public", express.static("public"))
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 })
