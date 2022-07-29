3f4cb0446ed8679ea0fea76f940e60ffc641d793
masa-1234tf <deathjuly88@gmail.com>
2022-07-29 16:39:55 +0900

    add

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var express = require("express")
 var app = express()
 app.use("/public", express.static("public"))
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html')
 })
 
 app.listen(port, () => {
-    console.log('server listening. Port:' + port);
-});
+    console.log('server listening. Port:' + port)
+})
 
