15fe60bb9b31eac94f25a444d93f78067584075f
masa-1234tf <deathjuly88@gmail.com>
2022-08-13 10:10:22 +0900

    add

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,29 @@
-$(function(){
-  var win = $(window);
-  var nav = $('nav');
-  var navPosi = $('nav').offset().top;
-  win.on('scroll',function(){
-    var scr = win.scrollTop();
-    if(scr > navPosi){
-        nav.addClass('sticky');
-        $('#page-top').fadeIn('slow');
-    }else{
-        nav.removeClass('sticky');
-        $('#page-top').fadeOut('slow');
-    }
-  });
+$(function () {
+    var win = $(window);
+    var nav = $('nav');
+    var navPosi = $('nav').offset().top
+    
+
+        $('.dropdwn li').click(function () {
+            $("ul:not(:animated)", this).slideDown()
+        }, function () {
+            $("ul.dropdwn_menu", this).slideUp()
+        })
+
+    win.on('scroll', function () {
+        var scr = win.scrollTop()
+        if (scr > navPosi) {
+            nav.addClass('sticky')
+            $('#page-top').fadeIn('slow')
+        } else {
+            nav.removeClass('sticky')
+            $('#page-top').fadeOut('slow')
+        }
+    })
     $('#move-page-top').click(
         function () {
             // スムーズにスクロールする
             $('html,body').animate({ scrollTop: 0 }, 'slow');
         }
-    );
-});
+    )
+})
--- server.js
+++ server.js
@@ -10,5 +10,6 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
     console.log('server listening. Port:' + port)
+    console.log(`http://localhost:${port}`)
 })
 
